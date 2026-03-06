db33d5cb1d99acb66ec4ea1d294b1e60123c2913
Manh-CEO <ongnoitv3@gmail.com>
2026-03-06 08:44:46 +0700

    v2 Uniform interface

--- Week2/client.py
+++ Week2/client.py
@@ -1,5 +1,10 @@
 import requests
 
+
+
+
 response = requests.get('http://localhost:5000')
 print(response.text)
 
+response = requests.get('http://localhost:5000/books')
+print(f"get all books: {response.json()}")
--- Week2/server.py
+++ Week2/server.py
@@ -1,10 +1,34 @@
 from flask import Flask
+from dataclasses import dataclass
 
 app = Flask(__name__)
+
+@dataclass
+class Book:
+    id: int
+    title: str
+    author: str
+    price: float
+
+
+books = [
+    Book(id=1, title="Book 1", author="Author 1", price=10.0),
+    Book(id=2, title="Book 2", author="Author 2", price=20.0),
+    Book(id=3, title="Book 3", author="Author 3", price=30.0),
+]
+
+
+
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/books')
+def get_books():
+    return [book.__dict__ for book in books]
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, )
